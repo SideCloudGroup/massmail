5196de04294bf44dbefe28ef3465175f1fd49359
022by <46434255+pplulee@users.noreply.github.com>
2023-02-21 21:11:21 +0000

    改为使用Postal API发信

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # massmail
 邮件群发
-从csv中导入邮箱，批量通过SMTP发信
+从csv中导入邮箱，批量通过Postal API发信 \
+使用前请在主程序（main.py）内设置Postal API信息与发件人信息 \
+邮箱列表（TXT文件）格式为一行一个邮箱 \
+邮件内容需要HTML文件
--- main.py
+++ main.py
@@ -1,43 +1,63 @@
-import smtplib
-import email
-from email import encoders
-from email.header import Header
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.utils import parseaddr, formataddr
+import json
 
-smtp_server = smtplib.SMTP("SMTP server here", 25)  # SMTP服务器地址与端口
-# smtp_server.set_debuglevel(1) #调试
-smtp_server.login("Username", "Password")  # SMTP用户名与密码
-from_addr = ""  # 发件人邮箱
-from_name = ""  # 发件人名
+from requests import post
+
+server = ""  # Postal面板地址，无需路径和斜杠
+key = ""  # Postal API Key
+from_name = ""  # 发件人名称
+from_email = ""  # 发件人邮箱
 
 
-def _format_addr(s):  # 编码
-    name, addr = parseaddr(s)
-    return formataddr((Header(name, 'utf-8').encode(), addr))
+def send_mails(to, subject, content):
+    global server, key, from_name, from_email
+    data = {
+        "from": from_email,
+        "sender": from_name,
+        "to": to,
+        "subject": subject,
+        "html_body": content
+    }
+    # 转换成json格式
+    data = json.dumps(data)
+    result = post(server + "/api/v1/send/message", data=data,
+                  headers={"X-Server-API-Key": key, "content-type": "application/json"})
+    if result.status_code == 200:
+        result_json = json.loads(result.text)
+        if result_json["status"] != "success":
+            print(f"发件失败，错误信息：{result_json['data']['message']}")
+            return False
+        else:
+            print("发件成功")
+            return True
+    else:
+        print(f"发件失败，错误信息：{result.text}")
+        return False
 
 
-def SendMail(toaddr):
-    message = MIMEMultipart()
-    title = "邮件标题"
-    mailtextfile = open("邮件模板.html", "r", encoding='UTF-8')
-    mailbody = mailtextfile.read()
-    mailtextfile.close()
-    message.attach(MIMEText(mailbody, 'html', 'utf-8'))
-    message['From'] = _format_addr(f"{from_name} <%s>" % from_addr)
-    message['To'] = _format_addr(f"{toaddr} <%s>" % f"{toaddr}")
-    message['Subject'] = Header(f'{title}', 'utf-8').encode()
-    smtp_server.sendmail(from_addr, toaddr, message.as_string())
+# 读取邮件列表到数组
+emails = []
+emails_file_name = input("请输入邮箱列表txt文件名，无需后缀\n")
+try:
+    emails_file = open(emails_file_name + ".txt", "r")
+except FileNotFoundError:
+    print("读取邮箱列表失败")
+    exit()
+else:
+    for line in emails_file.readlines():
+        if line.strip() != "":
+            emails.append(line.strip())
+    emails_file.close()
 
+print(f"共读取到{len(emails)}个邮箱")
+html_file_name = input("请输入邮件内容html文件名，无需后缀\n")
+try:
+    html_file = open(html_file_name + ".html", "r")
+except FileNotFoundError:
+    print("读取邮件内容失败")
+    exit()
+else:
+    html = html_file.read()
+    html_file.close()
 
-def ReadCSV(filepath):
-    with open(filepath) as OriginCSV:
-        OriginCSV.readline()
-        for line in OriginCSV:
-            print(line)
-            toaddr = line.split(",")[0]
-            SendMail(toaddr)
-
-
-smtp_server.quit()
+title = input("请输入邮件标题\n")
+send_mails(emails, title, html)
