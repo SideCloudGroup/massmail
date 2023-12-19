917d202df923b17875a9aab1698923e5499da93a
pplulee <46434255+pplulee@users.noreply.github.com>
2023-12-19 13:09:23 +0800

    feat: 增加配置文件和延迟发信

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# massmail
-邮件群发
-从csv中导入邮箱，批量通过Postal API发信 \
-使用前请在主程序（main.py）内设置Postal API信息与发件人信息 \
-邮箱列表（TXT文件）格式为一行一个邮箱 \
-邮件内容需要HTML文件
+# Postal邮件群发工具
+
+## 使用方法：
+1. 在`config.toml`中配置好邮件服务器的信息，以及发件相关设置
+2. 将收件人的邮箱地址填写到`emails.txt`中，每行一个
+3. 将邮件内容以HTML的格式写入`email.html`中
+4. 执行`pip3 install -r requirements.txt`安装依赖
+5. 执行`python3 main.py`开始发送邮件
--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import json
+import time
 
+import toml
 from requests import post
-
-server = ""  # Postal面板地址，无需路径和斜杠
-key = ""  # Postal API Key
-from_name = ""  # 发件人名称
-from_email = ""  # 发件人邮箱
+from tqdm import tqdm
 
 
 def send_mails(to, subject, content):
-    global server, key, from_name, from_email
     data = {
         "from": f"{from_name} <{from_email}>",
         "sender": from_email,
@@ -34,36 +31,50 @@ def send_mails(to, subject, content):
         return False
 
 
-# 读取邮件列表到数组
-emails = []
-emails_file_name = input("请输入邮箱列表txt文件名，无需后缀\n")
-try:
-    emails_file = open(emails_file_name + ".txt", "r")
-except FileNotFoundError:
-    print("读取邮箱列表失败")
-    exit()
-else:
-    for line in emails_file.readlines():
-        if line.strip() != "":
-            emails.append(line.strip())
-    emails_file.close()
-    if len(emails) == 0:
-        print("邮箱列表为空")
+if __name__ == "__main__":
+    try:
+        config = toml.load("config.toml")
+    except FileNotFoundError:
+        print("配置文件config.toml不存在")
         exit()
-
-print(f"共读取到{len(emails)}个邮箱")
-html_file_name = input("请输入邮件内容html文件名，无需后缀\n")
-try:
-    html_file = open(html_file_name + ".html", "r", encoding="utf-8")
-except FileNotFoundError:
-    print("读取邮件内容失败")
-    exit()
-else:
-    html = html_file.read()
-    html_file.close()
-
-title = input("请输入邮件标题\n")
-if input("确认发送？(y/n)") != "y":
-    exit()
-for address in emails:
-    send_mails([address], title, html)
+    server = config["postal"]["server"]
+    key = config["postal"]["key"]
+    from_name = config["postal"]["from_name"]
+    from_email = config["postal"]["from_email"]
+    if server == "" or key == "" or from_name == "" or from_email == "":
+        print("请先修改配置文件config.toml")
+        exit()
+    # 读取邮件列表到数组
+    emails = []
+    try:
+        emails_file = open(config['setting']['email_list'], "r")
+    except FileNotFoundError:
+        print("读取邮箱列表失败")
+        exit()
+    else:
+        for line in emails_file.readlines():
+            if line.strip() != "":
+                emails.append(line.strip())
+        emails_file.close()
+        if len(emails) == 0:
+            print("邮箱列表为空")
+            exit()
+    print(f"共读取到{len(emails)}个邮箱")
+    try:
+        html_file = open(config["setting"]["email_content"], "r", encoding="utf-8")
+    except FileNotFoundError:
+        print("读取邮件内容失败")
+        exit()
+    else:
+        html = html_file.read()
+        html_file.close()
+    if input("确认发送？(y/n)") != "y":
+        exit()
+    # 计算延迟
+    delay = (60 / config["setting"]["limit"]) if config["setting"]["limit"] != 0 else 0
+    total_emails = len(emails)
+    print(f"延迟{delay}秒发送，共{total_emails}封邮件")
+    for i, address in enumerate(tqdm(emails, desc="发送邮件进度"), start=1):
+        send_mails([address], config["setting"]["subject"], html)
+        if i != total_emails:
+            time.sleep(delay)
